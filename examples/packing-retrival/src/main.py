18c93c27e1721fb703ca2d5994c7f84e6798539c
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-06-03 15:43:38 -0500

    boiler plate setup w/error

---
+++ examples/packing-retrival/src/main.py
@@ -0,0 +1,16 @@
+from memary.agent.chat_agent import ChatAgent
+
+system_persona_txt = "data/system_persona.txt"
+user_persona_txt = "data/user_persona.txt"
+past_chat_json = "data/past_chat.json"
+memory_stream_json = "data/memory_stream.json"
+entity_knowledge_store_json = "data/entity_knowledge_store.json"
+
+chat_agent = ChatAgent(
+    "Personal Agent",
+    memory_stream_json,
+    entity_knowledge_store_json,
+    system_persona_txt,
+    user_persona_txt,
+    past_chat_json,
+)
